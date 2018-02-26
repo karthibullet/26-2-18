e5dbb9cdbfa990f2613eb2c140086da9db35ce7b
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-02-26 16:07:55 +0530

    66.c

--- 66.c
+++ 66.c
@@ -1,23 +1,20 @@
-#include<stdio.h>
-int main()
+
+#include <stdio.h>
+void main()
 {
-int n,i,a,count=0;
-printf("enter the number");
-scanf("%d",&n);
-for(i=2;i<n;i++)
-{
-a=n%i;
-if(a==0)
-{
-count++;
-}
-if((count==0)||(count==1))
-{
-printf("%d is prime no",n);
-}
+	int n,i,flag=0;
+	printf("enter the value of n");
+	scanf("%d",&n);
+	for(i=2;i<=n%2;++i)
+	{
+		if(n%i==0)
+		{
+			flag=1;
+			break;
+		}
+	}
+if(flag==0)
+printf(" prime %d",n);
 else
-{
-printf("%d is not a prime number",n);
-}
- return 0;
+printf("not prime",n);
 }
