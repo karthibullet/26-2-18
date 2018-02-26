235f0390cc3842180c7064bf61bbb6cc6dbf529e
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-02-26 16:51:08 +0530

    74.c

---
+++ 74.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+float n;
+clrscr();
+printf("Enter the number:-");
+ scanf("%f",&n);
+printf("The integer number after rounded is  %5.0f",n);
+return 0;
+}
